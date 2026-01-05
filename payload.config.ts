c1da8ef53f484fa48bc782f970837183492a6d4e
umarabid123 <muhammadali786.mak@gmail.com>
2026-01-05 14:40:28 +0500

    refactor: Use environment variables for MongoDB connection URL with local fallback.

--- payload.config.ts
+++ payload.config.ts
@@ -103,8 +103,7 @@ export default buildConfig({
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
   db: mongooseAdapter({
-    // url: 'mongodb://127.0.0.1:27017/mosque-al-falah',
-    url: 'mongodb+srv://mosque-admin:mosque123@cluster0.oggca09.mongodb.net/mosque-al-falah',
+    url: process.env.DATABASE_URL || process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mosque-al-falah',
   }),
   sharp,
   plugins: [
