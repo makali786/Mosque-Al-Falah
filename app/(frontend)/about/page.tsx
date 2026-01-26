276bc5068efd948075babd0d26e715e6377725f5
Muhammad Umar <143098591+umarabid123@users.noreply.github.com>
2026-01-26 19:06:17 +0500

    fix/about-us-page-text-and-spacing-issue-resolve (#44)

--- app/(frontend)/about/page.tsx
+++ app/(frontend)/about/page.tsx
@@ -23,7 +23,7 @@ export default async function AboutUsPage() {
   // Mission Content
   const missionContent = (
     <div className="flex flex-col gap-4">
-      <p className="text-base leading-relaxed">
+      <p className="text-lg">
         {aboutUs?.mission?.introduction}
       </p>
       {aboutUs?.mission?.missionPoints && (
