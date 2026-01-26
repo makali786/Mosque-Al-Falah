0e278bb15983ad968e49847d58867d54b6e16953
umarabid123 <umarabid709@gmail.com>
2026-01-26 12:53:50 +0500

    fix/prayar-time-page-spacing-issue

--- app/(frontend)/components/prayer-times/PrayerTimesWrapper.tsx
+++ app/(frontend)/components/prayer-times/PrayerTimesWrapper.tsx
@@ -117,26 +117,24 @@ export default function PrayerTimesWrapper({
 
   return (
     <>
-      <div className="flex flex-col lg:flex-row justify-between items-center mb-6 gap-4 section-padding pt-8">
+      <div className="flex flex-col lg:flex-row justify-between items-center gap-4 section-padding pt-8">
         {/* Toggle Switch */}
         <div className="bg-[#FAFAFA] p-1 rounded-lg flex w-full flex-wrap md:w-auto">
           <button
             onClick={() => setActiveTab("prayer-time")}
-            className={`flex-1 md:flex-none px-4 md:px-10 py-2.5 rounded-md text-sm md:text-base font-medium transition-all md:min-w-[174px] md:max-w-[174px] cursor-pointer text-center whitespace-nowrap ${
-              activeTab === "prayer-time"
+            className={`flex-1 md:flex-none px-4 md:px-10 py-2.5 rounded-md text-sm md:text-base font-medium transition-all md:min-w-[174px] md:max-w-[174px] cursor-pointer text-center whitespace-nowrap ${activeTab === "prayer-time"
                 ? "bg-white text-[#18181B] shadow-sm"
                 : "text-[#71717A] hover:text-[#18181B]"
-            }`}
+              }`}
           >
             Prayer Time
           </button>
           <button
             onClick={() => setActiveTab("calendar")}
-            className={`flex-1 md:flex-none px-4 md:px-10 py-2.5 rounded-md text-sm md:text-base font-medium transition-all md:min-w-[174px] md:max-w-[174px] cursor-pointer text-center whitespace-nowrap ${
-              activeTab === "calendar"
+            className={`flex-1 md:flex-none px-4 md:px-10 py-2.5 rounded-md text-sm md:text-base font-medium transition-all md:min-w-[174px] md:max-w-[174px] cursor-pointer text-center whitespace-nowrap ${activeTab === "calendar"
                 ? "bg-white text-[#18181B] shadow-sm"
                 : "text-[#71717A] hover:text-[#18181B]"
-            }`}
+              }`}
           >
             Calendar
           </button>
@@ -152,9 +150,8 @@ export default function PrayerTimesWrapper({
             >
               <span>Download {displayYear} timetable</span>
               <IoChevronDown
-                className={`w-4 h-4 transition-transform ${
-                  isDownloadOpen ? "rotate-180" : ""
-                }`}
+                className={`w-4 h-4 transition-transform ${isDownloadOpen ? "rotate-180" : ""
+                  }`}
               />
             </button>
 
